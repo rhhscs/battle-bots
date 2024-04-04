0fc4d27801c513f8cf19c03736e9c71424c73a95
withercraft303 <lsntao.withercraft303@gmail.com>
2024-04-04 17:21:59 -0400

    pathfind

--- src/battle/bots/game/GameMap.java
+++ src/battle/bots/game/GameMap.java
@@ -2,11 +2,17 @@ package battle.bots.game;
 
 
 import battle.bots.game.actions.Move;
+import battle.bots.game.actions.Move.Direction;
 import battle.bots.game.objects.Bot;
 import battle.bots.game.objects.GameObject;
 
 import java.awt.Point;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Queue;
 
 /**
  * An object which provides information about the map to the player.
@@ -15,8 +21,10 @@ import java.util.List;
  */
 public class GameMap {
     private final GameObject[][] map;
+    private final Map<Point, Map<Point, Move>> pathfindCache = new HashMap<>();
     private final Bot bot;
     private final Point botPosition;
+
 
     public GameMap(GameObject[][] map, Bot bot, Point botPosition) {
         this.map = map;
@@ -28,13 +36,59 @@ public class GameMap {
         return this.botPosition;
     }
 
-    public Move pathfind(Point dest) {
-        return this.pathfind(dest.x, dest.y);
+    public Move pathfind(Point start, Point dest) {
+        if (map[start.y][start.x] == null || map[dest.y][dest.x] == null)
+            return null;
+        if (!pathfindCache.containsKey(start) || !pathfindCache.get(start).containsKey(dest))
+            return null;
+        return pathfindCache.get(start).get(dest);
     }
 
-    public Move pathfind(int x, int y) {
-        // TODO: implement pathfinding
-        return null;
+
+    private void constructPathfind() {
+        for (int y = 0; y <= map.length; y++){
+            for (int x = 0; x <= map[y].length; x++){
+                if (map[y][x] != null) {
+                    constructPathfindSingle(new Point(x, y));
+                }
+            }
+        }
+    }
+    private void constructPathfindSingle(Point tile) {
+        Map<Point, Move> res = new HashMap<>();
+        Queue<Point> q = new LinkedList<Point>();
+        HashSet<Point> visited = new HashSet<Point>();
+        q.add(tile);
+        while (!q.isEmpty()) {
+            Point curr = q.remove();
+            if (inBounds(curr.y - 1, curr.x) && map[curr.y - 1][curr.x] != null) {
+                Point temp = new Point(curr.x, curr.y - 1);
+                res.put(temp, new Move(Direction.NORTH));
+                q.add(temp);
+            }
+            if (inBounds(curr.y + 1, curr.x) && map[curr.y + 1][curr.x] != null) {
+                Point temp = new Point(curr.x, curr.y + 1);
+                res.put(temp, new Move(Direction.SOUTH));
+                q.add(temp);
+            }
+            if (inBounds(curr.y, curr.x - 1) && map[curr.y][curr.x - 1] != null) {
+                Point temp = new Point(curr.x - 1, curr.y);
+                res.put(temp, new Move(Direction.WEST));
+                q.add(temp);
+            }
+            if (inBounds(curr.y, curr.x + 1) && map[curr.y][curr.x + 1] != null) {
+                Point temp = new Point(curr.x + 1, curr.y);
+                res.put(temp, new Move(Direction.EAST));
+                q.add(temp);
+            }
+
+            visited.add(curr);
+        }
+        pathfindCache.put(tile, res);
+    }
+    
+    private boolean inBounds(int x, int y){
+        return false;
     }
 
     public List<GameObject> getObjects() {
@@ -42,3 +96,5 @@ public class GameMap {
         return null;
     }
 }
+
+
