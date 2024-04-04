129c0f3b3afce97a6a4fdc47907741d60f61e0a2
Andrew Chu <67875282+andrewchu16@users.noreply.github.com>
2024-04-04 16:27:07 -0400

    changed file explorer appearance

--- src/battle/bots/Main.java
+++ src/battle/bots/Main.java
@@ -3,9 +3,18 @@ package battle.bots;
 import battle.bots.ui.BattleBotsApplication;
 
 import java.io.IOException;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class Main {
     public static void main(String[] args) {
+
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | UnsupportedLookAndFeelException | InstantiationException |
+                 IllegalAccessException ex) {
+            ex.printStackTrace();
+        }
         try {
             BattleBotsApplication battleBots = new BattleBotsApplication();
             battleBots.start();
